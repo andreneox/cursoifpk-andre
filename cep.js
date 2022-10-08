226d7a72fd98b6b6df520881c67c15c77d0e636e
Michel Paiva <m.michel.paiva@gmail.com>
2022-10-08 17:08:01 -0400

    Site AppLab

--- cep.js
+++ cep.js
@@ -7,13 +7,16 @@ function buscarCEP() {
     fetch(url).then (response => response.json())
         .then (response =>{
             console.log(response)
-            if (response.cep){
+            
+            if (response.cep)
+            {
                 document.getElementById('estado').value = response.state
                 document.getElementById('cidade').value = response.city
                 document.getElementById('rua').value = response.street
                 document.getElementById('bairro').value = response.neighborhood
             }
-            else{
+            else
+            {
                 document.getElementById('estado').removeAttribute('readonly')
                 document.getElementById('cidade').removeAttribute('readonly')
                 document.getElementById('rua').removeAttribute('readonly')
