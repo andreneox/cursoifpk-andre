64a2f9bb37f0b04252758909d37466052746083e
Andre Costa <andreneox@gmail.com>
2022-12-17 15:27:42 -0400

    Aula

--- react/aula03/src/pages/Login.js
+++ react/aula03/src/pages/Login.js
@@ -1,9 +1,45 @@
+import axios from "axios"
+import { useState } from "react"
+
 const Login = () => {
-    return(
+
+    const [login, setLogin] = useState()
+    const [senha, setSenha] = useState()
+
+    const enviar = (event) => {
+        event.preventDefault()
+
+        axios.post('http://localhost:3000/login',{
+            email: login,
+            password: senha
+        }) .then (({data})=>{
+            console.log(data)
+        }) .catch ((error)=>{
+            console.log('Login ou Senha Incorreto')
+        })
+
+    }
+
+    return (
         <div>
             <h2>
-                Login
+                Página de Login
             </h2>
+            <form onSubmit={enviar}>
+
+                <div>
+                    <label>Usuário: </label>
+                    <input type="text" onChange={(e) => setLogin(e.target.value)}></input>
+                </div>
+                <br></br>
+                <div>
+                    <label>Senha: </label>
+                    <input type="password" onChange={(e) => setSenha(e.target.value)}></input>
+                </div>
+                <br></br>
+                <button>LOGAR</button>
+
+            </form>
         </div>
     )
 }
