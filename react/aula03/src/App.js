2df1825d6398012848678a9edaf16d7bf4164eaf
Andre Costa <andreneox@gmail.com>
2022-12-17 14:20:32 -0400

    Aula

--- react/aula03/src/App.js
+++ react/aula03/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className='App'>
-      <h1> Topo da Pagina</h1>
+      <h1> Topo da Página</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/sobre" element={<Sobre/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
         </Routes>
       </BrowserRouter>
 
-    <h1>Rodape da Pagina</h1>
+    <h1>Rodape da Página</h1>
 
     </div>
   );
