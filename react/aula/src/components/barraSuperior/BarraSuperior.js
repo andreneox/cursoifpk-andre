cc2a1a5f81a13ec08077d4e83346db3f784a4ea9
Andre Costa <andreneox@gmail.com>
2023-01-07 17:13:53 -0400

    Primeira Aula de 2023 - 1000000

--- react/aula/src/components/barraSuperior/BarraSuperior.js
+++ react/aula/src/components/barraSuperior/BarraSuperior.js
@@ -7,10 +7,18 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useContext } from 'react';
 import { TitleContext } from '../../context/TitleContext';
+import { useNavigate } from 'react-router-dom';
 
 const BarraSuperior = () =>{
 
     const {title} = useContext(TitleContext);
+    const navigate =useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('token' )
+        localStorage.removeItem('logado')
+        navigate ('/login')
+    }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -28,7 +36,7 @@ const BarraSuperior = () =>{
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
-          <Button color="inherit">Sair</Button>
+          <Button color="inherit" onClick={handleLogout} >Sair</Button>
         </Toolbar>
       </AppBar>
     </Box>
--- react/aula/src/pages/pessoas/PessoasForm.js
+++ react/aula/src/pages/pessoas/PessoasForm.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import BarraSuperior from "../../components/barraSuperior/BarraSuperior"
 import VerificaLogin from "../../components/verificarLogin/VerificaLogin"
 import {TitleContext} from "../../context/TitleContext"
@@ -7,7 +7,11 @@ import {TitleContext} from "../../context/TitleContext"
 const PessoasForm = () => {
 
     const {setTitle} = useContext(TitleContext)
-
+    
+    useEffect(()=>{
+        setTitle('Cadastro de Pessoas')
+    
+    }, [setTitle])
     return(
         <VerificaLogin>
             <BarraSuperior/>
