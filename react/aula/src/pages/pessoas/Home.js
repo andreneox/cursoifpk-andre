226ac46f945e624e822dff69b3a27ff31c1c2beb
Andre Costa <andreneox@gmail.com>
2023-01-14 14:47:20 -0400

    update class

--- react/aula/src/pages/pessoas/Home.js
+++ react/aula/src/pages/pessoas/Home.js
@@ -2,13 +2,25 @@ import React, { useContext, useEffect } from 'react'
 import VerificaLogin from '../../components/verificarLogin/VerificaLogin'
 import BarraSuperior from '../../components/barraSuperior/BarraSuperior'
 import {TitleContext }from '../../context/TitleContext'
+import api from "../../services/Api"
 
 const Home = () => {
 
 const {setTitle} = useContext(TitleContext)
 
+
 useEffect(()=>{
     setTitle('Listagem de Pessoas')
+
+}, [setTitle])
+
+
+useEffect(()=>{
+    api.get ('api/', {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    }).then()
 
 }, [setTitle])
 
